5c5b1d5f02d003e7ac7535664d5e1d2b364a3135
D-bugging <archmelo@protonmail.com>
2026-06-07 18:16:34 -0300

    feat: adiciona nova entidade e migration

--- Taskei.API/Data/AppDbContext.cs
+++ Taskei.API/Data/AppDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Taskei.API.Entities;
 
 namespace Taskei.API.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<TaskItem> TaskItems { get; set; }
     }
 }
