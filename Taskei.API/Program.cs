955e8ec2d0b98e9e3c2ff5314d612197f87e03b3
D-bugging <archmelo@protonmail.com>
2026-06-07 17:55:34 -0300

    feat: adiciona conexão com banco de dados e configura swagger

--- Taskei.API/Program.cs
+++ Taskei.API/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Taskei.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
 //app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
